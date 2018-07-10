cc090804b719927e82f3f6ed0798bcf0c6972bb2
blkscorp33 <don.berzley@gmail.com>
2018-07-09 21:17:52 -0400

    Add Gcam and CameraRoll to prebuilt

--- common.mk
+++ common.mk
@@ -172,7 +172,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    Snap
+    GoogleCamera \
+    CameraRoll
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
